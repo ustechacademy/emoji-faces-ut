0ed4b1e710d29aae506a04d35fcef84170ffa21c
ustechacademy <ustechacademytool@gmail.com>
2023-05-23 22:39:46 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,96 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button } from "@thirdweb-dev/react";
 import "./styles/Home.css";
+import { useContract, useContractWrite, useAddress, useClaimedNFTSupply, useUnclaimedNFTSupply, useActiveClaimConditionForWallet } from "@thirdweb-dev/react";
+import preview from "./images/preview.gif"
+import { useState } from "react";
+const nftDropContractAddress = "0xe6F62bA51429c1293eb9B806e44474ba93806cDA";
 
-export default function Home() {
+function Home() {
+
+  const { contract: nftDrop } = useContract(nftDropContractAddress);
+  const address = useAddress();
+
+  console.log("address", address);
+
+  const [quantity, setQuantity] = useState(1);
+
+  const unclaimedSupply = useUnclaimedNFTSupply(nftDrop);
+  const claimedSupply = useClaimedNFTSupply(nftDrop);
+
+  const activeClaimCondition = useActiveClaimConditionForWallet(nftDrop, address);
+
+  console.log("activeClaimCondition", activeClaimCondition?.data?.currencyMetadata.displayValue);
+
+  console.log("unclaimedSupply", Number(unclaimedSupply.data));
+
+  console.log("claimedSupply", Number(claimedSupply.data));
+
   return (
     <div className="container">
-      <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
-        </p>
-
-        <div className="connect">
-          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center'}} />
+      <main className="mintInfoContainer">
+        <div className="imageSide">
+          <img
+            className="image"
+            src={preview}
+            alt="Emoji faces NFT Preview"
+          />
         </div>
 
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
+        <div className="mintCompletionArea">
+          <div className="mintAreaLeft">
+            <h2>Total Minted</h2>
+          </div>
 
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
+          <div className="mintAreaRight">
             <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
+              <b>{Number(claimedSupply.data)}/{Number(unclaimedSupply?.data) + Number(claimedSupply?.data)} </b>
             </p>
-          </a>
+          </div>
 
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+          <div>
+            <h2>Quantity</h2>
+            <div className="quantityContainer">
+              <button
+                className="quantityControlButton"
+                onClick={() => setQuantity(quantity - 1)}
+                disabled={quantity <= 1}
+              >
+                -
+              </button>
+              <h4>{quantity}</h4>
+              <button
+                className="quantityControlButton"
+                onClick={() => setQuantity(quantity + 1)}
+                disabled={quantity >= Number(activeClaimCondition.data?.maxClaimablePerWallet)}
+              > + </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="mintContainer">
+          {Number(unclaimedSupply?.data) + Number(claimedSupply?.data) == Number(claimedSupply.data) ?
+            <div>
+              <h2>Sold Out!!!!</h2>
+            </div> :
+            <Web3Button
+              contractAddress={nftDropContractAddress}
+              action={(contract) => contract.erc721.claim(quantity)}
+              onError={(err) => {
+                alert("Error minting NFTs", err.message);
+              }}
+              onSuccess={() => {
+                alert("Succesfully minted NFT");
+              }}
+            >
+              Mint NFT ({Number(activeClaimCondition?.data?.currencyMetadata.displayValue) * quantity}
+              {" "}{activeClaimCondition?.data?.currencyMetadata.symbol})
+            </Web3Button>
+          }
+
         </div>
       </main>
     </div>
   );
 }
+
+export default Home;
